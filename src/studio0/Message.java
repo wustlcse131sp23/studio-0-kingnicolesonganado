f4f78c6a9d082805e0c6bbf30cd122ede413c5b6
lydia <lydia@DESKTOP-ATVG590.dhcp.wustl.edu>
2023-01-19 13:49:26 -0600

    finished messages

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -4,6 +4,9 @@ public class Message {
 
 	public static void main(String args[]) {
 		System.out.println("Say hi to your group mates!");
-		System.out.println("heyyy")
+		System.out.println("heyyy");
+		System.out.println("hey!");
+		System.out.println("hi!!");
+		
 	}
 }
